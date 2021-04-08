32df1f6ad50c06786518ad0ac0e79161399c465f
NoeFabris <noe@elysresidence.com>
2021-04-08 18:16:40 +0200

    Aggiunti controlli, Fine

--- myscript.js
+++ myscript.js
@@ -1,22 +1,34 @@
 var km = prompt("Distance to travel in km");
 var age = prompt("Your Age");
-var price = (km * 0.21);
 console.log(km);
 console.log(age);
-console.log(price);
 
-if (age < 18) {
-    var price20 = ((price - (price * 20 / 100)).toFixed(2));
-    console.log(price20);
-    document.getElementById('price').innerHTML= price20 ;
-} else {
-    if (age < 65) {
-        console.log(price);
-        document.getElementById('price').innerHTML= price ;
+if ((km > 0) && (age >= 0)) {
+    if (age < 18) {
+        var price = km * 0.21;
+        var price20 = (price - (price * 20 / 100)).toFixed(2);
+        console.log(price20);
+        document.getElementById('price').innerHTML= price20 ;
     } else {
-        var price40 = ((price - (price * 40 / 100)).toFixed(2));
-        console.log(price40);
-        document.getElementById('price').innerHTML= price40 ;
+        if (age < 65) {
+            var price = km * 0.21;
+            console.log(price);
+            document.getElementById('price').innerHTML= price ;
+        } else {
+            var price = km * 0.21;
+            var price40 = (price - (price * 40 / 100)).toFixed(2);
+            console.log(price40);
+            document.getElementById('price').innerHTML= price40 ;
+        }
+    }
+} else {
+    if (isNaN(km) && isNaN(age)) {
+        document.getElementById('price').innerHTML= "Please use numbers" ;
+    } else if (isNaN(age)) {
+        document.getElementById('price').innerHTML= "Please insert AGE in numbers" ;
+    } else {
+        document.getElementById('price').innerHTML= "Please insert KM in numbers" ;
     }
 }
+
 
