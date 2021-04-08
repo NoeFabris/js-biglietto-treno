3d1e6ca242fa6a8606cc2e97ef13d96b89d44f54
NoeFabris <noe@elysresidence.com>
2021-04-08 15:47:04 +0200

    Creazione files

---
+++ myscript.js
@@ -0,0 +1,5 @@
+// var myname = prompt("Insert your name");
+// var mysurname = prompt("Now your surname");
+// var mycolor = prompt("Now your favorite colour");
+
+// document.getElementById('pw-gen').innerHTML= myname + mysurname + mycolor + "21";
