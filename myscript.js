a6df23ac2d419e310f7ce9a95b58622018311a0b
NoeFabris <noe@elysresidence.com>
2021-04-08 17:24:31 +0200

    Formula funzionante

--- myscript.js
+++ myscript.js
@@ -1,5 +1,22 @@
-// var myname = prompt("Insert your name");
-// var mysurname = prompt("Now your surname");
-// var mycolor = prompt("Now your favorite colour");
+var km = prompt("Distance to travel in km");
+var age = prompt("Your Age");
+var price = (km * 0.21);
+console.log(km);
+console.log(age);
+console.log(price);
 
-// document.getElementById('pw-gen').innerHTML= myname + mysurname + mycolor + "21";
+if (age < 18) {
+    var price20 = ((price - (price * 20 / 100)).toFixed(2));
+    console.log(price20);
+    document.getElementById('price').innerHTML= price20 ;
+} else {
+    if (age < 65) {
+        console.log(price);
+        document.getElementById('price').innerHTML= price ;
+    } else {
+        var price40 = ((price - (price * 40 / 100)).toFixed(2));
+        console.log(price40);
+        document.getElementById('price').innerHTML= price40 ;
+    }
+}
+
